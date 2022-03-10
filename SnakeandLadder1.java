d0d034c2a49e7412e1c2b19689e23ee0a9e59889
PoojaSolankar <poojasolankar09gmail.com>
2022-03-10 20:02:37 +0530

    Strat Game

---
+++ SnakeandLadder1.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz;
+
+public class SnakeandLadder1 {
+	
+
+	public static void main(String[] args) {
+		SnakeNLadder1 s = new SnakeNLadder1();
+		s.startGame();
+	}
+
+}
+class SnakeNLadder1
+{
+	public void startGame()
+	{
+		int player1 =0;
+		int currentPlayer=-1;
+	}
+}
